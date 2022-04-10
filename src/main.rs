8abd5e266d037ae39504626454ec3e93431e6616
AlexanderHOtt <aott01@protonmail.ch>
2022-04-09 21:14:09 -0700

    create base project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+mod models;
+mod schema;
+
 fn main() {
     println!("Hello, world!");
 }
