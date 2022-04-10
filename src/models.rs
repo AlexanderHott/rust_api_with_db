fb3154620b1efc08239bc5f54cf8c5c4d70fc5e3
AlexanderHOtt <aott01@protonmail.ch>
2022-04-09 21:22:31 -0700

    diesel setup

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,12 @@
 use diesel;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+
+#[derive(Queryable)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub published: bool,
+}
+
